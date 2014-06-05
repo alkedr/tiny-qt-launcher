078c9ba135cbe630e9dcccd21551087578a2681e
Alexander Kedrik <alkedr@yandex-team.ru>
2014-06-05 18:42:26 +0400

    load programs list from config

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <QtGui>
 #include <QtWidgets>
 
+static QStringList getCompleterList() {
+	QStringList result;
+
+	QFile configFile(QStringLiteral("/home/alkedr/.config/tiny-qt-launcher"));
+	if (!configFile.open(QIODevice::ReadOnly)) {
+		qWarning("Couldn't open config file.");
+		return result;
+	}
+	auto json = QJsonDocument(QJsonDocument::fromJson(configFile.readAll())).object();
+
+	QJsonArray levelArray = json["programs"].toArray();
+	for (QJsonValue programValue : levelArray) {
+		qDebug() << programValue.toString();
+		result.push_back(programValue.toString());
+	}
+
+	return result;
+}
+
 int main(int argc, char ** argv) {
 	QApplication app(argc, argv);
 	QApplication::setFont(QFont("Arial", 20));
 
 	auto lineEdit = new QLineEdit();
 	lineEdit->setAlignment(Qt::AlignHCenter);
-	auto completer = new QCompleter({"thunar", "firefox", "urxvt", "subl3", "chromium", "qbittorrent", "skype"});
+	auto completer = new QCompleter(getCompleterList());
 	completer->setCompletionMode(QCompleter::InlineCompletion);
 	lineEdit->setCompleter(completer);
 
